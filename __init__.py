e97b00c88b03ecfa92e3d8a059b6e6bdcbedcd58
Harikrishna Vardhineedi <harivardhineedi@gmail.com>
2025-04-07 17:17:32 -0700

    Implemented layer model for quantum circuits

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from qupy import gates as g
+from qupy import gates
 from qupy import layers
 from qupy.qubit import Qubit
 from qupy import states
+from qupy import subroutines
+from qupy import algorithms
--- algorithms/deutsch.py
+++ algorithms/deutsch.py
@@ -4,6 +4,9 @@ import qupy as qp
 def deutsch(f):
 
     def deutsch_Uf(f):
+        '''
+        Define the Deutsch Unitary
+        '''
         U = np.zeros((4, 4))
         for x in [0, 1]:
             for y in [0, 1]:
@@ -13,17 +16,11 @@ def deutsch(f):
                 U[output_index][input_index] = 1
         return U
 
-    zero = np.array([[1], [0]])
-    one = np.array([[0], [1]])
+    state = qp.layers.Input(2) # Initializes circuit with 2 qubits (all set to ket0)
+    state = qp.layers.Custom(state, qp.subroutines.gate_builder([qp.gates.I, qp.gates.X])) # Set the last qubit to ket1
+    state = qp.layers.N_Hadamard(state) # Apply Hadamard on all the qubits to get ket+ and ket-
+    state = qp.layers.Custom(state, deutsch_Uf(f)) # Apply the Deutsch unitary to entire circuit
+    state = qp.layers.Custom(state, qp.subroutines.gate_builder([qp.gates.H, qp.gates.I])) # Apply Hadamard to only the first qubit
+    state = qp.layers.Measure(state, [0]) # Measure the first qubit to get the result
 
-    init_state = np.kron(zero, one)
-
-    after_hadamard = np.kron(qp.gates.H, qp.gates.H) @ init_state
-
-    after_Uf = deutsch_Uf(f) @ after_hadamard
-
-    hadamard_back = np.kron(qp.gates.H, qp.gates.I) @ after_Uf
-
-    measured, new = qp.Qubit.partial_measure(hadamard_back, [0])
-
-    return 'odd' if int(measured) == 1 else 'even'
+    return 'balanced' if int(state[0]) == 1 else 'constant' # If the measurement is ket1, the function is balanced. If ket0, the function is constant
